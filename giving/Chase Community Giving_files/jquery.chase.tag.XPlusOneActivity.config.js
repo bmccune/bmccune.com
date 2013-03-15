8b325c762e96da3da2a73a09341af42ce7547648
Brian McCune <brian@bmccune.com>
2013-03-15 13:39:57 -0700

    Migratation to Node

--- giving/Chase Community Giving_files/jquery.chase.tag.XPlusOneActivity.config.js
+++
@@ -1,2 +0,0 @@
-// JavaScript Document
-var configurl ="https://s.xp1.ru4.com/";
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Brian McCune - Web Developer, Designer, Front-End Expert, and Web Marketer' });
+};
